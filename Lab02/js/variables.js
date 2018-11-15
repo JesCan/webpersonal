bd2288b4fa0d76a76d3a939c2b9dcee73cd0e59a
Monitor <jesuscanales@outlook.com>
2018-11-15 13:24:59 -0500

    Laboratorio 02 trabajando con variables

---
+++ Lab02/js/variables.js
@@ -0,0 +1,102 @@
+//Definiendo variables
+var nombre = 'Fernando';
+
+//Imprimir contenido de la variables nomnbre
+console.log(nombre);
+
+//Definiendo datos personales con variables
+var nomPersona = 'Gloria';
+var apePersona = 'Guerra Paz';
+var edadPersona = 20;
+var procPersona = 'San Vicente';
+
+//Imprimir datos personales
+console.log('Mi nombre es ' + nomPersona + ' y mis apellidos son ' + apePersona);
+console.log('Mi edad es ' + edadPersona + ' y soy del distrito de ' + procPersona);
+
+//Definiendo variables numéricas y cálculos respectivos
+var numUno = 100;
+var numDos = 350;
+var suma = numUno + numDos;
+var resta = numDos - numUno;
+var producto = numUno * numDos;
+var divide = numDos / numUno;
+var mayor = numDos > numUno;
+var comparar = numUno == numDos;
+
+//Imprimir resultados
+console.log('La suma del ' + numUno + ' más ' + numDos + ' es ' + suma);
+console.log('La resta de ' + numDos + ' menos ' + numUno + ' es ' + resta);
+console.log('La multiplicación de ' + numUno + ' por ' + numDos + ' es ' + producto);
+//Resolver
+var producto = 'pantalones jeans';
+var cantidad = 5;
+var precio = 125;
+var total = cantidad * precio;
+
+//Imprimir mensaje de resultado
+console.log('El precio a pagar por el producto ' + producto + ' es ' + total);
+//Definir array con los días de la semana
+var diasSemana = ['Lunes','Martes','Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo'];
+
+//Imprimiendo el contenido de la variable de tipo array
+console.log(diasSemana);
+console.log(diasSemana[4]);
+console.log(diasSemana[2]);
+
+//Crear un array con 5 nombres de personas e imprimirlos en pantalla grupal e individual
+var amigos = ['Luis', 'Fernando', 'Marcos', 'Martín', 'Juan'];
+console.log(amigos);
+console.log(amigos[0]);
+console.log(amigos[1]);
+console.log(amigos[2]);
+console.log(amigos[3]);
+console.log(amigos[4]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- readme.md
+++ readme.md
@@ -4,3 +4,4 @@ Empezaremos a conocer el lenguaje de programación JavaScript.
 
 **Descripción de laboratorios**
 - **Laboratorio 01:** En este laboratorio hemos vinculado un archivo JS y además hemos impreso texto y números en la consola del navegador.
+- **Laboratorio 02:** En este laboratorio hemos trabajado con variables de tipo texto, número y array.

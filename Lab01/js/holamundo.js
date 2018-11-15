2353fcd4c575e0ad4017bab9e271b2c9b0761362
Monitor <jesuscanales@outlook.com>
2018-11-15 12:03:54 -0500

    He agregado más ejercicios en Hola Mundo

--- Lab01/js/holamundo.js
+++ Lab01/js/holamundo.js
@@ -15,3 +15,20 @@ console.log(100 + 500);
 
 //Comparar los siguientes números 800 y 500
 console.log(800 < 500);
+
+//Imprime 3 nombres de países
+console.log('Perú, Brasil, Bolivia');
+
+//Imprimir mensaje de error
+console.error('Este error es de prueba... no te asustes.. todo va a estar bien..')
+
+//Imprimir mensaje de advertencia
+console.warn('Cuidado... te estoy vigilando...');
+
+//Imprimir mensaje de información
+console.info('Este es un aviso de JavaScript');
+
+//Multiplicar 3 números: 10 X 25 X 105
+console.log(10*25*105);
+
+

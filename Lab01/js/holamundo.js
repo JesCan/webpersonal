cb3b1917f3359b87919125bb3e098d123292f856
Monitor <jesuscanales@outlook.com>
2018-11-15 10:01:14 -0500

    he creado el laboratorio 01

---
+++ Lab01/js/holamundo.js
@@ -0,0 +1,17 @@
+//Mi primer mensaje por consola
+console.log('Hola terrícola... te escribo desde el más allá...');
+
+//Imprimir otro mensaje de prueba...
+console.log('Este es otro mensaje... no te olvides como hacerlo....')
+
+//Imprimir números
+console.log(3.580);
+
+//Imprimir 5 nombres de distritos
+console.log('San Vicente, Imperial, Mala, Asia y Bujama');
+
+//Sumar los números 100 y 500
+console.log(100 + 500);
+
+//Comparar los siguientes números 800 y 500
+console.log(800 < 500);
--- readme.md
+++ readme.md
@@ -1,3 +1,6 @@
 # Mis primeros pasos con JavaScript
 
 Empezaremos a conocer el lenguaje de programación JavaScript.
+
+**Descripción de laboratorios**
+- **Laboratorio 01:** En este laboratorio hemos vinculado un archivo JS y además hemos impreso texto y números en la consola del navegador.
